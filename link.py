e616cc11933c7ab041b19d0f4ab092b80961b055
Ubada Raja <ubada_21@hotmail.com>
2021-04-30 05:08:19 -0700

    fixed Car class functionality

--- link.py
+++ link.py
@@ -1,14 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
 
 class Car:
     def __init__(self, link):
 
         self.link = link
-        self.car_attrs = str(link.split("<span>"))
-        self.car_attributes = []
-        for i in self.car_attrs:
-            self.car_attributes.append(i.split(':'))
-        self.car_attributes = self.car_attributes[1:]
-#        self.year = attr['yr']
-#        self.transmission = attr['trasnmission']
-#        self.odometer = attr['odometer']
-#        self.title_status = attr['title status']
+        self.title = 'hello'
+        link_info = requests.get(link).text
+        car_soup = BeautifulSoup(link_info, 'html.parser').find_all(class_='attrgroup')
+        car_title = car_soup[0]
+        car_attr = car_soup[1]
+        car_attrs = str(car_attr).split('<span>')
+        car_attributes = []
+        for i in car_attrs:
+            car_attributes.append(i.split(':'))
+
+        car_attributes = car_attributes[1:]
+
+        for j in car_attributes:
+            j[1] = j[1].split("<b>")[1].split("</b>")[0]
+
+
+        self.title = str(car_title).split('<span>')[1].split('<b>')[1].split('</b>')[0]
+        self.attrbt_dict = dict(car_attributes)
+
+        self.fuel = self.attrbt_dict['fuel']
+        self.odometer = self.attrbt_dict['odometer']
+        self.title_status = self.attrbt_dict['title status']
+        self.transmission = self.attrbt_dict['transmission']
+
+
+
+
--- miata-finder.py
+++ miata-finder.py
@@ -1,8 +1,7 @@
 from link import Car
 from bs4 import BeautifulSoup
 import requests
-import html.parser
-import re
+
 """
 city = input("Enter City: ")
 model = input("Enter Car Model: ")
@@ -24,7 +23,9 @@ elif trans == 'manual':
                                   "max_auto_miles=" + max_km +"&" \
                                   "auto_transmission=" + str(trans)"""
 
-craigs_link = 'https://vancouver.craigslist.org/search/cta?query=850i&purveyor-input=all'
+craigs_link = 'https://vancouver.craigslist.org/search/cta?query=850i&purveyo' \
+              'r-input=all'
+
 page = requests.get(craigs_link).text
 
 soup = BeautifulSoup(page, 'html.parser')
@@ -42,64 +43,12 @@ for link in links_only:
 all_links = list(dict.fromkeys(all_links))
 print(all_links)
 
-print(len(all_links))
-
-link = all_links[0]
-
-car_link = requests.get(link).text
-car_soup = BeautifulSoup(car_link, 'html.parser')
-# print(car_soup)
-
-car_links = car_soup.find_all(class_='attrgroup')
-#print(car_links)
-
-#car_attr = car_links[1]
-#car_attrs = str(car_attr).split("<span>")
-#car_attributes = []
-#for link in car_attrs:
-#    car_attributes.append(link.split(':'))
-
-#car_attributes = car_attributes[1:]
-
-#for attrbt in car_attributes:
-#    attrbt[1] = attrbt[1].split("<b>")[1].split("</b>")[0]
-
-car_attributes2 = []
+car_list = []
 
 for link in all_links:
-    link = BeautifulSoup(requests.get(link).text, 'html.parser').find_all(class_='attrgroup')
-    link = str(link).split('<span>')
-    for l in link:
-        l = l.split(':')
-    #    #car_attributes2.append(l)
-    car_attributes2.append(link)
-car_attributes3 = []
-car_attributes5 = []
-
-for i in car_attributes2:
-    car_attributes3.append(i)
+    car = Car(link)
+    car_list.append(car)
 
 
-for i in car_attributes3:
-    for item in range(len(i)):
-        i[item] = i[item].split(':')
-
-
-dict1 = []
-
-for i in car_attributes3:
-    cardict = dict(i[2:])
-    dict1.append(cardict)
-
-print(dict1)
-#print(attr_dict)
-
-
-# make class with each individual link, all formatting happens within class?
-cars = []
-#for link in all_links:
-#    car = Car(link)
-#    cars.append(car)
-
-car1 = Car(all_links[0])
-
+for car in car_list:
+    print(car.title)
