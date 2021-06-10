5bb3dc7d214aa87ea68336b0025ef25ea48a4440
Ubada Raja <ubada_21@hotmail.com>
2021-06-10 11:22:33 -0700

    Final changes

--- miata_finder.py
+++ miata_finder.py
@@ -5,6 +5,6 @@ import settings as s
 try:
     while True:
         s.scrape_link(s.craigs_link)
-        time.sleep(10)
+        time.sleep(600) # time between scrapes in seconds.
 except KeyboardInterrupt:
     pass
